e04e9969d71bde9ed9650b991444be84de1d4292
sathyadevops <sathyadevops1@gmail.com>
2019-12-04 15:38:59 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,11 @@
+class demo
+{
+  public static void main()
+  {
+    printf(" Hello world");
+    printf(" Hello world");
+    printf(" Hello world");
+    printf(" Hello world");
+    printf(" Hello world");
+  }
+}
